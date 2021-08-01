a8026e0c48ea51454be1f3a04521ce2e74274123
lingniker <lingyuanping@outlook.com>
2021-08-01 13:42:09 +0800

    commit

--- server/routes/operation.js
+++ server/routes/operation.js
@@ -2,10 +2,10 @@ var express = require('express');
 var router = express.Router();
 var exec = require('child_process').exec;
 
-var shStr = 'sh ./bin/deploy.sh';
+var shStr = 'sh ./script/deploy.sh';
 
 /* GET home page. */
-router.get('/operation', function(req, res, next) {
+router.get('/', function(req, res, next) {
   // console.log('node 热跟新 -----', req.query);
   // console.log('get req.query', req.query);
   exec(shStr ,function(error,stdout,stderr){
@@ -15,15 +15,15 @@ router.get('/operation', function(req, res, next) {
 });
 
 /* GET home page. */
-router.post('/operation', function(req, res, next) {
-  // console.log('post req.body', req.body);
+router.post('/', function(req, res, next) {
+  console.log('post req.body', req.body);
   // console.log('只有 web-dev 分支才会自动更新 -- 暂时先不考虑自动话更新的事情')
-  if (req.body.ref.indexOf('web-dev') > -1) {
+  // if (req.body.ref.indexOf('web-dev') > -1) {
+    // }
     exec(shStr ,function(error,stdout,stderr){
       console.log(error,stdout,stderr)
     })
-    res.json(req.body);
-  }
+  res.json(req.body);
 });
 
 module.exports = router;
