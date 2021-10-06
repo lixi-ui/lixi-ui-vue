0276ce29569859af30bf8a208fa185825bdd1582
lingniker <lingyuanping@outlook.com>
2021-10-07 07:47:41 +0800

    提交

--- src/components/tree/doc/index.vue
+++ src/components/tree/doc/index.vue
@@ -22,11 +22,36 @@
   }
 </script>
 
-<style>
+<style lang="scss">
  .lx-button{
    margin-right: 5px;
  }
  .block{
    margin: 10px;
  }
+  .custom-tree-node {
+    flex: 1;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    font-size: 14px;
+    padding-right: 8px;
+  }
+  .custom-tree-container {
+    display: flex;
+    /* margin: -24px; */
+  }
+  .custom-tree-container .block{
+    width: 50%;
+    padding: 8px 24px 24px;
+    p{
+      text-align: center;
+      margin: 0;
+      line-height: 4;
+    }
+  }
+
+  .block:first-child{
+    border-right: 1px solid #eff2f6;
+  }
 </style>
